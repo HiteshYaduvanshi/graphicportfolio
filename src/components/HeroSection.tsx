12d9b59d5b1c90c86b9d10709ede3b21e40aebb7
hitesh yaduvanshi <hiteshyaduvanshi85@gmail.com>
2024-06-22 19:27:37 +0530

    ' removed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,12 +9,12 @@ function HeroSection() {
       <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
         <div className="max-w-3xl mx-auto p-4">
           <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-            I'm ROHAN YADAV
+            I am ROHAN YADAV
           </h1>
           <p></p>
           <p className="text-neutral-500 max-w-full mx-auto my-2 text-sm text-center relative z-10">
 
-            I'm a trendy and innovative graphic designer and 3D artist based in India. With years of experience in the industry, I have worked with a variety of clients, delivering exceptional designs that exceed expectations. My expertise includes logo design, branding, illustration, 3D modeling, animation, social media designs, banner designs, t-shirt designs, template designs, and product designing and modeling services. My goal is to help businesses turn their ideas into a living brand, and I achieve this by collaborating closely with the clients to understand their vision and bring it to life through his unique creative skills.
+            I am a trendy and innovative graphic designer and 3D artist based in India. With years of experience in the industry, I have worked with a variety of clients, delivering exceptional designs that exceed expectations. My expertise includes logo design, branding, illustration, 3D modeling, animation, social media designs, banner designs, t-shirt designs, template designs, and product designing and modeling services. My goal is to help businesses turn their ideas into a living brand, and I achieve this by collaborating closely with the clients to understand their vision and bring it to life through his unique creative skills.
           </p>
           <div className="w-full flex justify-center mt-4 items-center">
             <Button
