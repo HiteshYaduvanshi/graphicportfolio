21ef4a18705b9f3b5e9bd7c3dbd4efbebbcaa237
hitesh yaduvanshi <hiteshyaduvanshi85@gmail.com>
2024-06-26 14:10:12 +0530

    why choose us img update

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -1,52 +1,125 @@
 import React from "react";
 import { StickyScroll } from "./ui/sticky-scroll-reveal";
 import Image from "next/image";
+import threeDImg from "../../public/1.3D work/abandon house1.jpg"
+import socialMediaDesign from "../../public/2.social media/10.jpg"
+import tShirtDesign from "../../public/3.tshirt designs/3.png"
+import bannerDesign from "../../public/4.banner design/Stumble guys 2.png"
+import logoDesign from "../../public/5.logo design/Gentlemens street logo.jpg"
+import whiteBackground from "../../public/6.white background/white bg1.jpg"
+import posterDesign from "../../public/7.poster design/pizza poster.jpg"
+
 
 const content = [
     {
-        title: "Collaborative Editing",
+        title: "3D Design",
         description:
             "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
         content: (
             <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-                Collaborative Editing
-            </div>
-        ),
-    },
-    {
-        title: "Real time changes",
-        description:
-            "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
-        content: (
-            <div className="h-full w-full  flex items-center justify-center text-white">
-                {/* <Image
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXert6ekqTL1J7LVWNAhs65_GeostayFqh6A&s"
+                <Image
+                    src={threeDImg}
                     width={300}
                     height={300}
                     className="h-full w-full object-cover"
                     alt="linear board demo"
-                /> */}
-                Version control
+                />
             </div>
         ),
     },
     {
-        title: "Version control",
+        title: "Social Media Design",
+        description:
+            "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+        content: (
+            <div className="h-full w-full  flex items-center justify-center text-white">
+                <Image
+                    src={socialMediaDesign}
+                    width={300}
+                    height={300}
+                    className="h-full w-full object-cover"
+                    alt="linear board demo"
+                />
+            </div>
+        ),
+    },
+    {
+        title: "T-Shirt Design",
         description:
             "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
         content: (
             <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
-                Version control
+                <Image
+                    src={tShirtDesign}
+                    width={300}
+                    height={300}
+                    className="h-full w-full object-cover"
+                    alt="linear board demo"
+                />
             </div>
         ),
     },
     {
-        title: "Running out of content",
+        title: "Banner Design",
         description:
             "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
         content: (
             <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-                Running out of content
+                <Image
+                    src={bannerDesign}
+                    width={300}
+                    height={300}
+                    className="h-full w-full object-cover"
+                    alt="linear board demo"
+                />
+            </div>
+        ),
+    },
+    {
+        title: "Logo Design",
+        description:
+            "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+        content: (
+            <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
+                <Image
+                    src={logoDesign}
+                    width={300}
+                    height={300}
+                    className="h-full w-full object-cover"
+                    alt="linear board demo"
+                />
+            </div>
+        ),
+    },
+    {
+        title: "White Background",
+        description:
+            "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+        content: (
+            <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
+                <Image
+                    src={whiteBackground}
+                    width={300}
+                    height={300}
+                    className="h-full w-full object-cover"
+                    alt="linear board demo"
+                />
+            </div>
+        ),
+    },
+    {
+        title: "Poster Design",
+        description:
+            "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+        content: (
+            <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
+                <Image
+                    src={posterDesign}
+                    width={300}
+                    height={300}
+                    className="h-full w-full object-cover"
+                    alt="linear board demo"
+                />
             </div>
         ),
     },
@@ -55,7 +128,7 @@ const content = [
 export function WhyChooseUs() {
     return (
         <div className="w-full py-20">
-        <StickyScroll content={content} />
+            <StickyScroll content={content} />
         </div>
     );
 }
