25902ebf8b89bf934fc7c9f2ac73f168ab7d546f
hitesh yaduvanshi <hiteshyaduvanshi85@gmail.com>
2024-07-14 22:21:36 +0530

    hero update

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,22 +1,30 @@
-"use client";
 import React from "react";
-import { Boxes } from "../components/ui/background-boxes";
-import { cn } from "@/utils/cn";
-import Heading from "./Heading";
-import { HeroText } from "./HeroText";
+import { Vortex } from "../components/ui/vortex";
 
-export function HeroSection({}) {
+export function HeroSection() {
   return (
-    <div className="h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
-      <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-
-      <Boxes />
-      <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
-        <HeroText />
-      </h1>
-      <p className="text-center mt-2 w-1/2  text-[#74777D] relative z-20">
-        I am a trendy and innovative graphic designer and 3D artist based in India. With years of experience in the industry, I have worked with a variety of clients, delivering exceptional designs that exceed expectations. My expertise includes logo design, branding, illustration, 3D modeling, animation, social media designs, banner designs, t-shirt designs, template designs, and product designing and modeling services. My goal is to help businesses turn their ideas into a living brand, and I achieve this by collaborating closely with the clients to understand their vision and bring it to life through his unique creative skills.
-      </p>
+    <div className="w-[calc(100%-2px)] mx-auto rounded-md  h-screen overflow-hidden">
+      <Vortex
+        backgroundColor="black"
+        rangeY={800}
+        particleCount={500}
+        baseHue={120}
+        className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
+      >
+        <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+          The hell is this?
+        </h2>
+        <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
+          This is chemical burn. It&apos;ll hurt more than you&apos;ve ever been
+          burned and you&apos;ll have a scar.
+        </p>
+        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
+          <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
+            Order now
+          </button>
+          <button className="px-4 py-2  text-white ">Watch trailer</button>
+        </div>
+      </Vortex>
     </div>
   );
 }
