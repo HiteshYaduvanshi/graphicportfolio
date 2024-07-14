e9c6620187fbee48edb749e3f2bbfadc4fdb7de2
hitesh yaduvanshi <hiteshyaduvanshi85@gmail.com>
2024-07-14 23:08:44 +0530

    error removed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -16,7 +16,7 @@ export function HeroSection() {
         <HeroText />
         <p className="text-[#747679] text-xs md:text-lg max-w-4xl mt-6 text-center">
 
-          I'm a trendy and innovative graphic designer and 3D artist based in India. With years of experience in the industry, I have worked with a variety of clients, delivering exceptional designs that exceed expectations. My expertise includes logo design, branding, illustration, 3D modeling, animation, social media designs, banner designs, t-shirt designs, template designs, and product designing and modeling services. My goal is to help businesses turn their ideas into a living brand, and I achieve this by collaborating closely with the clients to understand their vision and bring it to life through his unique creative skills.
+          I am a trendy and innovative graphic designer and 3D artist based in India. With years of experience in the industry, I have worked with a variety of clients, delivering exceptional designs that exceed expectations. My expertise includes logo design, branding, illustration, 3D modeling, animation, social media designs, banner designs, t-shirt designs, template designs, and product designing and modeling services. My goal is to help businesses turn their ideas into a living brand, and I achieve this by collaborating closely with the clients to understand their vision and bring it to life through his unique creative skills.
         </p>
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
           <HeroBtn/>
